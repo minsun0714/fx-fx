4e7933ecd49bdc4ef1161571000d443be10b311d
이민선 <woooooooow22@gmail.com>
2025-08-15 13:26:52 +0900

    first commit

---
+++ src/main/java/com/fx/newfeed/FxNewfeedApplication.java
@@ -0,0 +1,13 @@
+package com.fx.newfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxNewfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxNewfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fx/newfeed/FxNewfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fx.newfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxNewfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
