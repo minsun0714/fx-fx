23c7174b8f94934d474755499d8dbd64dbb15925
이민선 <woooooooow22@gmail.com>
2025-08-16 15:47:42 +0900

    feat(kafka): @EnableKafka 추가로 Kafka 리스너 활성화
    - KafkaConfig 클래스 생성(@Configuration, @EnableKafka)
    - @KafkaListener 기반 리스너 동작 활성화

---
+++ src/main/java/com/fx/newfeed/config/KafkaConfig.java
@@ -0,0 +1,9 @@
+package com.fx.newfeed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+}
