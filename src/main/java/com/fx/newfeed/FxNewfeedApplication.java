4b209482cc669c44271354bca6181b229ab54a93
이민선 <woooooooow22@gmail.com>
2025-08-16 15:48:34 +0900

    feat(scheduling): @EnableScheduling 추가로 스케줄러 활성화
    - @EnableScheduling 적용하여 @Scheduled 작업 실행 가능
    - 주기성 작업(예: 환율 수집/전송 등) 자동 트리거 활성화
    - 향후 스레드 풀/프리픽스 등 스케줄링 설정과 연동 예정

--- src/main/java/com/fx/newfeed/FxNewfeedApplication.java
+++ src/main/java/com/fx/newfeed/FxNewfeedApplication.java
@@ -2,12 +2,13 @@ package com.fx.newfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FxNewfeedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FxNewfeedApplication.class, args);
 	}
-
 }
